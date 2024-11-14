7914b1d2fc3ee309b6e9306d3d87688bebe55851
DmitriiDudarow <dudarow.d@yandex.ru>
2024-11-14 21:34:49 +0500

    Dudarow task 3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,24 +1,48 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
+        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"),
+                Charset.forName("windows-1251"));
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
-
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        //ключ — отсортированные буквы
+        Map<String, Set<String>> anagrams = new TreeMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                String[] words = line.toLowerCase().split("\\s+");
+
+                for (String word : words) {
+                    // Убираем всё, что короче 3 символов или не русские буквы
+                    if (word.length() >= 3 && word.matches("[а-я]+")) {
+                        // Сортируем буквы в слове по алфавиту
+                        char[] chars = word.toCharArray();
+                        Arrays.sort(chars);
+                        String sortedWord = new String(chars);
+
+                        anagrams.computeIfAbsent(sortedWord, k -> new TreeSet<>()).add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return anagrams.values().stream().filter(group -> group.size() >= 2) // Берём только группы с 2+ словами
+                .collect(Collectors.toList());
     }
+
+
 }
